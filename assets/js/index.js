b2bbf7109006bf6ae307a45644e3a9c0c8fc6049
Matt Bush <bush.matthewjames@gmail.com>
2019-02-28 12:38:51 +0000

    Add modal to contact form on contact.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,14 @@
 // Jquery learnt from Jquery module in Code Institutes course
 
 // Use Jquery to only show modal on form submit
-$(".form-navbar").on("submit", function(e){
+$(".form-navbar , .contact-form").on("submit", function(e){
     $("#registerModal").modal("show");
     e.preventDefault();
 });
+
+// $(".contact-form").on("submit", function(e){
+//     $("#contactModal").modal("show");
+//     e.preventDefault();
+// });
+
+
