864dbf6a86126098e902e8bfc7ae506514cefdf5
Matt Bush <bush.matthewjames@gmail.com>
2019-02-28 15:31:03 +0000

    Add smooth scrolling from #top icon

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,4 +8,16 @@ $(".form-navbar , .contact-form").on("submit", function(e){
 
 
 
-
+/*
+* Smooth scrolling to top when to-top icon is clicked
+* Learnt from https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
+*/
+$('a[href^="#"]').on('click', function(e) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        e.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
